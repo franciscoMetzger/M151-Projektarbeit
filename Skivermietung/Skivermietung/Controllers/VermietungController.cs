cce2f4e62924b87f811aeb2249a11fa72a471e96
Francisco Metzger <francisco.metzger@gmail.com>
2017-06-10 11:53:09 +0200

    Vermietung

--- Skivermietung/Skivermietung/Controllers/VermietungController.cs
+++ Skivermietung/Skivermietung/Controllers/VermietungController.cs
@@ -7,116 +7,125 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using Skivermietung.Business.Domain;
+using Skivermietung.Business.Domain.Repositories.Interfaces;
 using Skivermietung.Models;
 
 namespace Skivermietung.Controllers
 {
-	public class KundeController : Controller
+	public class VermietungController : Controller
 	{
 		private readonly IUnitOfWork _unitOfWork;
 
-		public KundeController(IUnitOfWork unitOfWork)
+		private readonly List<Kunde> _kundenSelection;
+
+		public VermietungController(IUnitOfWork unitOfWork)
 		{
 			_unitOfWork = unitOfWork;
+
+			_kundenSelection = new List<Kunde>(_unitOfWork.Kunde.LoadAll());
 		}
 
-		// GET: Kunde
+		// GET: Vermietung
 		public ActionResult Index()
 		{
-			return View(_unitOfWork.Kunde.LoadAll());
+			return View(_unitOfWork.Vermietung.LoadAll());
 		}
 
-		// GET: Kunde/Details/5
+		// GET: Vermietung/Details/5
 		public ActionResult Details(int? id)
 		{
 			if (id == null)
 			{
 				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
 			}
-			Kunde kunde = _unitOfWork.Kunde.Load(id.Value);
-			if (kunde == null)
+			Vermietung vermietung = _unitOfWork.Vermietung.Load(id.Value);
+			if (vermietung == null)
 			{
 				return HttpNotFound();
 			}
-			return View(kunde);
+			return View(vermietung);
 		}
 
-		// GET: Kunde/Create
+		// GET: Vermietung/Create
 		public ActionResult Create()
 		{
+			ViewBag.KundeId = new SelectList(_kundenSelection, "ID_Kunde", "Vorname");
 			return View();
 		}
 
-		// POST: Kunde/Create
+		// POST: Vermietung/Create
 		// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
 		// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
 		[HttpPost]
 		[ValidateAntiForgeryToken]
-		public ActionResult Create([Bind(Include = "ID_Kunde,Vorname,Name,TelefonNr,Geburtstag")] Kunde kunde)
+		public ActionResult Create([Bind(Include = "ID_Vermietung,Von,Bis,Bezahlt,Rabatt,KundeId")] Vermietung vermietung)
 		{
 			if (ModelState.IsValid)
 			{
-				_unitOfWork.Kunde.Insert(kunde);
+				_unitOfWork.Vermietung.Insert(vermietung);
 				_unitOfWork.SaveChanges();
 				return RedirectToAction("Index");
 			}
 
-			return View(kunde);
+			ViewBag.KundeId = new SelectList(_kundenSelection, "ID_Kunde", "Vorname", vermietung.KundeId);
+			return View(vermietung);
 		}
 
-		// GET: Kunde/Edit/5
+		// GET: Vermietung/Edit/5
 		public ActionResult Edit(int? id)
 		{
 			if (id == null)
 			{
 				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
 			}
-			Kunde kunde = _unitOfWork.Kunde.Load(id.Value);
-			if (kunde == null)
+			Vermietung vermietung = _unitOfWork.Vermietung.Load(id.Value);
+			if (vermietung == null)
 			{
 				return HttpNotFound();
 			}
-			return View(kunde);
+			ViewBag.KundeId = new SelectList(_kundenSelection, "ID_Kunde", "Vorname", vermietung.KundeId);
+			return View(vermietung);
 		}
 
-		// POST: Kunde/Edit/5
+		// POST: Vermietung/Edit/5
 		// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
 		// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
 		[HttpPost]
 		[ValidateAntiForgeryToken]
-		public ActionResult Edit([Bind(Include = "ID_Kunde,Vorname,Name,TelefonNr,Geburtstag")] Kunde kunde)
+		public ActionResult Edit([Bind(Include = "ID_Vermietung,Von,Bis,Bezahlt,Rabatt,KundeId")] Vermietung vermietung)
 		{
 			if (ModelState.IsValid)
 			{
-				_unitOfWork.Kunde.Delete(kunde);
+				_unitOfWork.Vermietung.Update(vermietung);
 				_unitOfWork.SaveChanges();
 				return RedirectToAction("Index");
 			}
-			return View(kunde);
+			ViewBag.KundeId = new SelectList(_kundenSelection, "ID_Kunde", "Vorname", vermietung.KundeId);
+			return View(vermietung);
 		}
 
-		// GET: Kunde/Delete/5
+		// GET: Vermietung/Delete/5
 		public ActionResult Delete(int? id)
 		{
 			if (id == null)
 			{
 				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
 			}
-			Kunde kunde = _unitOfWork.Kunde.Load(id.Value);
-			if (kunde == null)
+			Vermietung vermietung = _unitOfWork.Vermietung.Load(id.Value);
+			if (vermietung == null)
 			{
 				return HttpNotFound();
 			}
-			return View(kunde);
+			return View(vermietung);
 		}
 
-		// POST: Kunde/Delete/5
+		// POST: Vermietung/Delete/5
 		[HttpPost, ActionName("Delete")]
 		[ValidateAntiForgeryToken]
 		public ActionResult DeleteConfirmed(int id)
 		{
-			Kunde kunde = _unitOfWork.Kunde.Load(id);
-			_unitOfWork.Kunde.Delete(kunde);
+			Vermietung vermietung = _unitOfWork.Vermietung.Load(id);
+			_unitOfWork.Vermietung.Delete(vermietung);
 			_unitOfWork.SaveChanges();
 			return RedirectToAction("Index");
 		}
